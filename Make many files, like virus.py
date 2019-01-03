5ca9e36d2393aaf58bbee4c34461b342258c530e
Kush Choudhary <kushchoudhary8@gmail.com>
2019-01-03 18:40:35 +0530

    Add files via upload

---
+++ Make many files, like virus.py
@@ -0,0 +1,14 @@
+import random
+
+for t in range(5): # Change iterating range to increase number of files maded
+    name=random.randrange(2)
+    file=open(str(name)+'.txt','w')
+
+    file.write('I am Kushal \n COOL')
+    file.close()
+
+    fileRead=open(str(name)+'.txt','r')
+    text_extract=fileRead.read()
+    print(text_extract)
+
+    fileRead.close()
